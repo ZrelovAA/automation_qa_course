63c12a086d86cdef4b6c52f5d09db11f194a7c6d
azrelov <qa.zrelov.aa@gmail.com>
2022-06-30 02:44:55 +0400

    Updated some files

--- conftest.py
+++ conftest.py
@@ -1,12 +1,13 @@
 import pytest
 from selenium import webdriver
-from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium.webdriver.chrome.service import Service as ChromiumService
 from webdriver_manager.chrome import ChromeDriverManager
+from webdriver_manager.core.utils import ChromeType
 
 
 @pytest.fixture(scope="function")
 def driver():
-	driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
+	driver = webdriver.Chrome(service=ChromiumService(ChromeDriverManager(chrome_type=ChromeType.CHROMIUM).install()))
 	driver.maximize_window()
 	yield driver
 	driver.quit()
