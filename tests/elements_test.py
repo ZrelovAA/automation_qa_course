f8e3d735b4bdb38e026aeca742f3e6a4c27bc23c
azrelov <qa.zrelov.aa@gmail.com>
2022-07-02 03:07:16 +0400

    Updated files with checkboxes test code

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -1,12 +1,12 @@
 import time
 
-from pages.elements_page import TextBoxPage
+from pages.elements_page import TextBoxPage, CheckBoxPage
 
 
 class TestElements:
 	class TestTextBox:
 
-		def test_tex_box(self, driver):
+		def test_text_box(self, driver):
 			text_box_page = TextBoxPage(driver, 'https://demoqa.com/text-box')
 			text_box_page.open()
 			full_name, email, current_address, permanent_address = text_box_page.fill_all_fields()
@@ -15,3 +15,12 @@ class TestElements:
 			assert email == output_email, "the email does not match"
 			assert current_address == output_cur_addr, "the current address does not match"
 			assert permanent_address == output_per_addr, "the permanent address does not match"
+
+	class TestCheckBox:
+
+		def test_check_box(self, driver):
+			check_box_page = CheckBoxPage(driver, 'https://demoqa.com/checkbox')
+			check_box_page.open()
+			check_box_page.open_full_list()
+			check_box_page.click_random_checkbox()
+			time.sleep(5)
