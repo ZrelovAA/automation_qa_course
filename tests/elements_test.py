71f74970470f73340f0a4316f6510813367b5b92
azrelov <qa.zrelov.aa@gmail.com>
2022-07-02 01:33:49 +0400

    Added data and generator files + changes in elements test file

--- tests/elements_test.py
+++ tests/elements_test.py
@@ -9,10 +9,9 @@ class TestElements:
 		def test_tex_box(self, driver):
 			text_box_page = TextBoxPage(driver, 'https://demoqa.com/text-box')
 			text_box_page.open()
-			text_box_page.fill_all_fields()
-			time.sleep(5)
-			output_name, output_email, output_cur_addr, output_per_addr = text_box_page.check_filled_form()
-			print(output_name)
-			print(output_email)
-			print(output_cur_addr)
-			print(output_per_addr)
+			full_name, email, current_address, permanent_address = text_box_page.fill_all_fields()
+			output_name, output_email, output_cur_addr, output_per_addr,  = text_box_page.check_filled_form()
+			assert full_name == output_name, "the name does not match"
+			assert email == output_email, "the email does not match"
+			assert current_address == output_cur_addr, "the current address does not match"
+			assert permanent_address == output_per_addr, "the permanent address does not match"
